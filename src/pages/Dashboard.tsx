5794e46e48896aa180b22c6a26852db25440c4ce
abharun <155776685+abharun@users.noreply.github.com>
2024-07-17 10:45:01 +0200

    get image code and generate pptx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,33 +1,52 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { DragElement } from "./DragElement";
 import { domToPng } from "modern-screenshot";
+import PptxGenJS from "pptxgenjs";
 
 export const Dashboard: React.FC = () => {
+  const [isImageLoaded, setImageLoaded] = useState<boolean>(false);
   const imgRef = useRef<HTMLImageElement>(null);
 
-  const downloadImage = async () => {
-    if (imgRef.current) {
+  const downloadImage = async (
+    ref: React.RefObject<HTMLImageElement>,
+  ): Promise<string | null> => {
+    if (ref.current) {
       try {
-        const dataUrl = await domToPng(imgRef.current);
+        const dataUrl = await domToPng(ref.current);
         return dataUrl;
-      }
-      catch (e) {
-        console.error('Error capturing image:', e);
+      } catch (error) {
+        console.error('Error capturing image:', error);
         return null;
       }
+    } else {
+      console.log('Waiting for images to load');
+      return null;
     }
   };
 
   const export2pptx = async () => {
-    const dataUrl = await downloadImage();
+    const dataUrl = await downloadImage(imgRef);
     if (dataUrl) {
-      const base64Data = dataUrl.split(',')[1];
-      console.log(base64Data);
+      const pptx = new PptxGenJS();
+      const slide = pptx.addSlide();
+      slide.addImage({
+        data: dataUrl,
+        x: 0,
+        y: 0,
+        w: 466 / 96,
+        h: 583 / 96,
+      });
+
+      pptx.writeFile({ fileName: "presentation.pptx" });
     }
   };
 
   return <div className="MainBoard">
-    <img ref={imgRef} src="https://campussuite-storage.s3.amazonaws.com/prod/1559029/da50a146-938b-11ea-9ce3-0abf9590ab1b/2655419/eba87f66-4cf3-11ee-a3cc-0a58a9feac02/optimizations/16" />
+    <div>
+      <img
+        ref={imgRef}
+        src="https://i.pinimg.com/originals/92/95/f5/9295f5ba3c7491bd971dc8fc15ea2487.png" />
+    </div>
     <button style={{ border: "2px solid #ff0000" }} onClick={export2pptx}>Download to pptx</button>
   </div>;
 };
